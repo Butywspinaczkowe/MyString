61b40958e3da674715c7348346fa8b300ff464d3
but <buty.wspinaczkowe@gmail.com>
2020-12-15 11:31:33 +0000

    Add project files.

---
+++ MyString/MyString.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace CP
+{
+    public class MyString
+    {
+        private char[] charArray;
+        private int length;
+
+        public char[] CharArray { get; set; }
+
+        public int Length
+        {
+            get { return LengthGetter(); }
+        }
+
+        public MyString()
+        { }
+
+        public MyString(char[] inputArray)
+        {
+            CharArray = inputArray;
+        }
+
+        public MyString(char character)
+        {
+            CharArray[0] = character;
+        }
+
+        private int LengthGetter()
+        {
+            int arrayLength = 0;
+            foreach (char element in CharArray)
+            {
+                arrayLength++;
+            }
+            return arrayLength;
+        }
+
+        public void Write()
+        {
+            Console.Write(CharArray);
+        }
+
+    }
+}
